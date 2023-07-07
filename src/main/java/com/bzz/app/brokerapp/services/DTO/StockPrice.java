255d1fb9e05b0412927251c95cec05fb5e7a68a8
AndyI0I <andreasffelder@hotmail.com>
2023-07-07 15:22:39 +0200

    stuff

---
+++ src/main/java/com/bzz/app/brokerapp/services/DTO/StockPrice.java
@@ -0,0 +1,56 @@
+package com.bzz.app.brokerapp.services.DTO;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "stock_prices")
+public class StockPrice {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "stock_id", nullable = false)
+    private Stock stock;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+}
